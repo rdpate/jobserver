5732722cd8476980e53a8240602c733dd632ea9d
Roger Pate <rogerpate@gmail.com>
2018-12-09 15:51:17 -0500

    rename _release to _release.sh

--- cmd/_release.sh
+++ cmd/_release.sh
@@ -24,4 +24,3 @@ else
         }
     trap release exit
     fi
-
--- test/100-basics/fd-gt-9.sh
+++ test/100-basics/fd-gt-9.sh
@@ -1,4 +1,7 @@
 # 0,1,2 are stdin,out,err; requiring 8 more FDs means we reach at least FD 10
+# this is really a test that either:
+# * /dev/fd exists
+# * /bin/sh handles FD redirection for FDs >9
 assert_success 1 \
     "$jobserver" init --new \
     "$jobserver" init --new \
