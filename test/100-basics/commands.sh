497a291d15218a2f7a4a5fdfca71df2c4fa31f51
Roger Pate <rogerpate@gmail.com>
2018-12-13 22:31:02 -0500

    add cmd/jobserver-sync-exec

--- test/100-basics/commands.sh
+++ test/100-basics/commands.sh
@@ -5,3 +5,6 @@ assert_fails 1 "$jobserver" init
 assert_fails 1 "$jobserver" init "$jobserver" init
 assert_success 1 "$jobserver" init "$jobserver" started
 assert_success 1 "$jobserver" init "$jobserver" init "$jobserver" started
+
+assert_success 1 "$jobserver" sync-exec true
+assert_fails 1 "$jobserver" init "$jobserver" sync-exec "$jobserver" started
