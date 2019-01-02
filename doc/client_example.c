527c2a19924b87a466cf71e3e1007618eb33651a
Roger Pate <rogerpate@gmail.com>
2019-01-02 06:13:46 -0500

    allow client_example to run in either mode

--- doc/client_example.c
+++ doc/client_example.c
@@ -1,21 +1,20 @@
 #include "../lib/jobserver.h"
 
 char const *commands[] = {
-    "sleep 0.5; echo abc",
-    "sleep 0.2; echo def",
-    "sleep 0.4; echo ghi",
-    "sleep 0.3; echo jkl",
-    "sleep 0.0; echo mno",
-    "sleep 0.1; echo xyz",
+    "sleep 0.6; echo abc",
+    "sleep 0.3; echo def",
+    "sleep 0.5; echo ghi",
+    "sleep 0.4; echo jkl",
+    "sleep 0.1; echo mno",
+    "sleep 0.2; echo xyz",
     0};
 
 #include <unistd.h>
 int main(int argc, char **argv) {
-    //jobserver_init_or_exec(argv);
-    jobserver_init_or_sync();
+    if (argc == 1) jobserver_init_or_exec(argv);
+    else jobserver_init_or_sync();
 
     for (char const **x = commands; *x; x ++) {
-        //sleep(1);
         jobserver_bg_spawn("/bin/sh", "-c", *x, 0);
         }
 
